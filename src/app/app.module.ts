0905d3e8f9a887e9a8be35bd6d5bd0779863e7d0
Liju-07 <65362509+Liju-07@users.noreply.github.com>
2022-10-09 21:29:27 +0530

    Revert "angular"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,42 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BloodComponent } from './blood/blood.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AddDonorComponent } from './add-donor/add-donor.component';
-import { SearchDonorComponent } from './search-donor/search-donor.component';
-import { DeleteDonorComponent } from './delete-donor/delete-donor.component';
-import { RouterModule, Routes } from '@angular/router';
-
-
-const appRoute:Routes=[
-  {
-    path:"blood",component:BloodComponent
-  },
-  {
-    path:"search",component:SearchDonorComponent
-  },
-  {
-    path:"delete",component:DeleteDonorComponent
-  },
-  {
-    path:"adddonor",component:AddDonorComponent
-  }
-]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BloodComponent,
-    NavbarComponent,
-    AddDonorComponent,
-    SearchDonorComponent,
-    DeleteDonorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoute)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
